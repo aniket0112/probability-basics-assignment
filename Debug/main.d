4790cbade22854f07434e7f7c5713d1e38c2f3c3
aniket0112 <aniketkok10@gmail.com>
2017-08-06 14:44:19 +0530

    Switched workspace to ubuntu

---
+++ Debug/main.d
@@ -0,0 +1,6 @@
+main.o: ../main.cpp ../preprocess_data.h \
+ /home/aniket/gnuplot-iostream/gnuplot-iostream.h
+
+../preprocess_data.h:
+
+/home/aniket/gnuplot-iostream/gnuplot-iostream.h:
---
+++ Debug/preprocess_data.d
@@ -0,0 +1,3 @@
+preprocess_data.o: ../preprocess_data.cpp ../preprocess_data.h
+
+../preprocess_data.h:
